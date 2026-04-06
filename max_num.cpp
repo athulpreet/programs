e3f3d98a0a055ac6a729317e49b74bd30410f215
Athul preet <preetathul@gmail.com>
2026-04-06 14:34:06 +0530

    Add program to find the maximum of two numbers

---
+++ max_num.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+
+//WAP to find the max of two numbers 
+int main(){
+
+using namespace std;
+  
+int num1=23,num2=30;
+
+
+if(num1>num2)
+
+cout<<"num1 is greater "<<endl;
+else
+cout<<"num2 is greater"<<endl;
+
+  
+
+    return 0;
+}
